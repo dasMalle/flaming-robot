886d4d2c6e51c491aff9e17a4c666cedd23e53b0
Julie Heyde <julie@joe-macbookpro.local>
2015-01-24 10:45:25 +0100

    Node system.

---
+++ webGLaudio/Assets/Project/Editor/NodeEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(Node))]
+public class NodeEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		Node myTarget = (Node)target;
+
+		if(GUILayout.Button("Generate id"))
+		{
+			myTarget.GenerateId ();
+		}
+
+		if(GUILayout.Button("Autoconnect nearest"))
+		{
+			myTarget.AutoConnectNearest();
+		}
+
+		if(GUILayout.Button("Update connections"))
+		{
+			myTarget.UpdateConnected ();
+		}
+
+		if(GUILayout.Button("Clear all connections"))
+		{
+			myTarget.ClearConnected ();
+		}
+	}
+
+	public void OnSceneGUI()
+	{
+		Node myTarget = (Node)target;
+		myTarget.DrawDebugs ();
+	}
+}
--- webGLaudio/Assets/Project/Scripts/Node.cs
+++ webGLaudio/Assets/Project/Scripts/Node.cs
@@ -4,7 +4,16 @@ using System.Collections.Generic;
 
 public class Node : MonoBehaviour
 {
+	enum Direction
+	{
+		NORTH,
+		SOUTH,
+		WEST,
+		EAST
+	}
+
 	public static List<Node> nodes = new List<Node>();
+	public static List<int> ids = new List<int>();
 
 	public int id;
 
@@ -13,10 +22,138 @@ public class Node : MonoBehaviour
 	public Node westNode;
 	public Node eastNode;
 
+	public float radius = 10.0f;
+
 	void Awake ()
 	{
-		id = nodes.Count;
 		nodes.Add (this);
 	}
 
+	public void DrawDebugs()
+	{
+		DebugDraw (northNode);
+		DebugDraw (southNode);
+		DebugDraw (westNode);
+		DebugDraw (eastNode);
+
+		Debug.DrawLine (transform.position, transform.position + transform.forward * radius, Color.cyan);
+	}
+
+	void DebugDraw(Node node)
+	{
+		if(node != null)
+		{
+			Debug.DrawLine(transform.position, node.transform.position, Color.red);
+		}
+	}
+
+	public void GenerateId()
+	{
+		bool generated = false;
+
+		while(!generated)
+		{
+			int newId = Random.Range(0, 999999);
+
+			if(!ids.Contains(newId))
+			{
+				id = newId;
+				generated = true;
+			}
+		}
+	}
+
+	public void AutoConnectNearest()
+	{
+		Collider[] hits = Physics.OverlapSphere (transform.position, radius);
+
+		for(int i = 0; i < hits.Length; ++i)
+		{
+			if(hits[i].gameObject == gameObject)
+			{
+				continue;
+			}
+
+			Vector3 angleVector = hits[i].transform.position - transform.position;
+			float angle = Vector3.Angle(Vector2.right, new Vector2(angleVector.x, angleVector.z));
+
+			if(transform.position.x < hits[i].transform.position.x)
+			{
+				if(angle <= 45.0f)
+				{
+					northNode = hits[i].GetComponent<Node>();
+				}
+				else if (angle > 45.0f && angle <= 135.0f)
+				{
+					eastNode = hits[i].GetComponent<Node>();
+				}
+				else
+				{
+					southNode = hits[i].GetComponent<Node>();
+				}
+
+				Debug.Log("Left side angle: " + angle);
+
+			}
+			else
+			{
+				if(angle <= 45.0f)
+				{
+					northNode = hits[i].GetComponent<Node>();
+				}
+				else if (angle > 45.0f && angle <= 135.0f)
+				{
+					westNode = hits[i].GetComponent<Node>();
+				}
+				else
+				{
+					southNode = hits[i].GetComponent<Node>();
+				}
+
+				Debug.Log("Right side angle: " + angle);
+			}
+
+		}
+	}
+
+	public void UpdateConnected()
+	{
+		UpdateNodeConnection (northNode, Direction.NORTH);
+		UpdateNodeConnection (southNode, Direction.SOUTH);
+		UpdateNodeConnection (westNode, Direction.WEST);
+		UpdateNodeConnection (eastNode, Direction.EAST);
+	}
+
+	void UpdateNodeConnection(Node node, Direction dir)
+	{
+		if(node != null)
+		{
+			switch(dir)
+			{
+			case Direction.NORTH:
+				node.southNode = this;
+				break;
+
+			case Direction.SOUTH:
+				node.northNode = this;
+				break;
+
+			case Direction.EAST:
+				node.westNode = this;
+				break;
+
+			case Direction.WEST:
+				node.eastNode = this;
+				break;
+			}
+		}
+	}
+
+	public void ClearConnected()
+	{
+		northNode = null;
+		southNode = null;
+		eastNode = null;
+		westNode = null;
+	}
 }
